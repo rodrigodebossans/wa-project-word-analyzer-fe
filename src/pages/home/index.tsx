f160f250d60b1e1f4cd39b13b92feab6676cec4a
Rodrigo Debossans <rodrigo.debossans@gmail.com>
2024-10-28 19:54:34 -0300

    fix(classification-tree): add empty state

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { WaHomeContainer } from './styled';
 import WapTree from '../../components/tree';
 import { Component } from 'react';
-import { Divider, Flex, FloatButton, Splitter, Tooltip } from 'antd';
+import { Divider, Flex, FloatButton, Splitter, Tooltip, Typography } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
 import Logo from '../../components/logo';
 
@@ -19,16 +19,14 @@ class Home extends Component {
               <Flex align="center" justify="center" vertical>
                 <Logo />
                 <Divider>Word Analyzer</Divider>
-                <p>
+                <Typography.Paragraph className='slogan'>
                   Build your classification tree and analyze and visualize frequencies in the <br /> hierarchy of words
                   in your documents.
-                </p>
+                </Typography.Paragraph>
               </Flex>
             </Splitter.Panel>
             <Splitter.Panel min="50%">
-              <Flex vertical>
                 <WapTree />
-              </Flex>
             </Splitter.Panel>
           </Splitter>
 
